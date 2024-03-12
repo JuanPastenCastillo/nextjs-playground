1f9d6d81a886c5b483a16372c48d7e0f00b5913a
Juan Pasten Castillo <juanpastencastillo@gmail.com>
2024-03-12 15:16:03 -0300

    Fix - Playwright should work

--- e2e/example2.spec.ts
+++
@@ -1,9 +0,0 @@
-// @ts-check
-import { expect, test } from "@playwright/test"
-
-test("has title", async ({ page }) => {
-  await page.goto("http://localhost:3000")
-
-  // Expect a title "to contain" a substring.
-  await expect(page.getByText("homdadae")).toBeDefined()
-})
