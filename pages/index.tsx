bade335200884ba8c57fc4bf901cf0e3d44fb9b2
Juan Pasten Castillo <juanpastencastillo@gmail.com>
2024-03-13 13:47:20 -0300

    Added - CI for Playwright

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,9 @@
-import { INDEX_HOME } from "@/components/home/HOME"
-import { useMediaQuery } from "@/utils/user-event/useResizeWindow"
-import { Inter } from "next/font/google"
+import { Index_Home } from "@/components/home/index_home"
 
-const inter = Inter({ subsets: ["latin"] })
-
-export default function Home() {
-  let getSize = useMediaQuery({})
-
+export default function index() {
   return (
     <>
-      {/* <Calculator />
-
-      <INDEX_HOME /> */}
-      <h1>Changes!</h1>
-      <INDEX_HOME />
-
-      {/* <KeyboardComponent /> */}
+      <Index_Home />
     </>
   )
 }
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test"
 
 /**
  * Read environment variables from file.
@@ -9,8 +9,11 @@ import { defineConfig, devices } from '@playwright/test';
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
+
+const PORT = process.env.PORT || 3000
+const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`
 export default defineConfig({
-  testDir: './e2e',
+  testDir: "./e2e",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -20,31 +23,41 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
+
+  testIgnore: ["*/*.test.ts", "*/*.test.tsx", "*/*.test.js", "*/*.test.jsx"],
+  testMatch: "*/*.spec.ts",
+
+  expect: {
+    /* This is the default actually. See for more details: https://playwright.dev/docs/test-configuration#expect-options */
+    timeout: 5000,
+  },
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
+    baseURL: BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
     },
 
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
     },
 
     /* Test against mobile viewports. */
@@ -70,8 +83,14 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
+  //   command: "npm run start",
+  //   url: "http://localhost:3000",
   //   reuseExistingServer: !process.env.CI,
   // },
-});
+  webServer: {
+    command: `npm dev`,
+    url: BASE_URL,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+})
